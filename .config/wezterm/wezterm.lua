c60069aded62c520e85a00e5601c6d72e3f663e3
vasujain275 <vasujain275@gmail.com>
2024-05-17 23:47:24 +0530

    Added xterm-256color support to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,9 +5,10 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Builtin Dark"
+config.term = "xterm-256color"
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 20
+config.font_size = 16
 
 config.enable_tab_bar = false
 
