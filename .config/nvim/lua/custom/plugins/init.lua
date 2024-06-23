f61fd1643f9183db63e3723cf4fb5a82b7d29594
vasujain275 <vasujain275@gmail.com>
2024-06-23 17:24:03 +0530

    refactor: moudlarized nvim conifg

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -12,6 +12,11 @@ return {
   },
   {
     'm4xshen/autoclose.nvim',
+    opts = {
+      keys = {
+        ['$'] = { escape = true, close = true, pair = '$$', disabled_filetypes = {} },
+      },
+    },
   },
   {
     'ThePrimeagen/vim-be-good',
