cf46923f56c5530da4052bdda02b258ed193d422
vasujain275 <vasujain275@gmail.com>
2024-06-24 13:10:00 +0530

    chore: changed fastfetch img and defualt apps

--- .config/nvim/lua/custom/configs/formatter.lua
+++ .config/nvim/lua/custom/configs/formatter.lua
@@ -6,9 +6,6 @@ local M = {
     typescript = {
       require('formatter.filetypes.typescript').prettier,
     },
-    ['*'] = {
-      require('formatter.filetypes.any').remove_trailing_whitespace,
-    },
   },
 }
 
